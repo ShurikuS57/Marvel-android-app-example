bc03963ad3eede1b27c56d1ef97de089cbe2d715
Alexander Maltsev <cawa007@gmail.com>
2018-09-11 10:43:42 +0300

    Refactoring FavouritesPresenter

--- app/src/main/java/ru/taptm/marvelcomicssample/comics/favourites/presenter/FavouritesPresenter.kt
+++ app/src/main/java/ru/taptm/marvelcomicssample/comics/favourites/presenter/FavouritesPresenter.kt
@@ -1,49 +1,35 @@
 package ru.taptm.marvelcomicssample.comics.favourites.presenter
 
-import android.net.Uri
 import com.arellomobile.mvp.InjectViewState
 import ru.taptm.marvelcomicssample.base.BasePresenter
-import ru.taptm.marvelcomicssample.comics.comicList.adapter.ComicsCell
 import ru.taptm.marvelcomicssample.comics.favourites.view.IFavouritesView
 import ru.taptm.marvelcomicssample.di.DI
-import ru.taptm.marvelcomicssample.reposetory.IAppDataStorage
-import ru.taptm.marvelcomicssample.reposetory.network.response.ImageResponse
-import ru.taptm.marvelcomicssample.reposetory.network.response.ResultResponse
+import ru.taptm.marvelcomicssample.interactor.FavouritesInteractor
 import javax.inject.Inject
 
 @InjectViewState
-class FavouritesPresenter: BasePresenter<IFavouritesView>() {
-
+class FavouritesPresenter : BasePresenter<IFavouritesView>() {
     @Inject
-    lateinit var repository: IAppDataStorage
+    lateinit var favouritesInteractor: FavouritesInteractor
 
     init {
         DI.componentManager().appComponent().inject(this)
     }
 
     fun loadData() {
-        disposable.add(repository.getAllFavourites().subscribe {
-            data ->
-            if (data.isNotEmpty()) {
-                val cells: ArrayList<ComicsCell> = ArrayList()
-                data.forEach {
-                    val resultResponse = ResultResponse()
-                    resultResponse.id = it.comicsId.toInt()
-                    resultResponse.title = it.title
-                    val uri = Uri.parse(it.imageUrl)
-                    resultResponse.images = arrayListOf(
-                            ImageResponse(uri.toString().replace("/portrait_xlarge.jpg", ""), "jpg")
-                    )
-                    val cell = ComicsCell(resultResponse)
-                    cell.setOnCellClickListener {result ->
-                        viewState.goToComicsDetailsScreen(result.id)
+        disposable.add(favouritesInteractor
+                .loadAllFavourites()
+                .subscribe { cellComicsList ->
+                    if (cellComicsList.isNotEmpty()) {
+                        cellComicsList.forEach { cellComics ->
+                            cellComics.setOnCellClickListener {
+                                viewState.goToComicsDetailsScreen(it.id)
+                            }
+                        }
+                        viewState.showComicsList(cellComicsList)
+                    } else {
+                        viewState.showPlaceHolder()
                     }
-                    cells.add(cell)
-                }
-                viewState.showComicsList(cells)
-            } else {
-                viewState.showPlaceHolder()
-            }
-        })
+                })
     }
 }
--- app/src/main/java/ru/taptm/marvelcomicssample/interactor/FavouritesInteractor.kt
+++ app/src/main/java/ru/taptm/marvelcomicssample/interactor/FavouritesInteractor.kt
@@ -1,7 +1,11 @@
 package ru.taptm.marvelcomicssample.interactor
 
+import android.net.Uri
+import ru.taptm.marvelcomicssample.comics.comicList.adapter.ComicsCell
 import ru.taptm.marvelcomicssample.reposetory.IAppDataStorage
 import ru.taptm.marvelcomicssample.reposetory.local.FavouritesData
+import ru.taptm.marvelcomicssample.reposetory.network.response.ImageResponse
+import ru.taptm.marvelcomicssample.reposetory.network.response.ResultResponse
 
 class FavouritesInteractor(private val repository: IAppDataStorage) {
 
@@ -14,4 +18,24 @@ class FavouritesInteractor(private val repository: IAppDataStorage) {
     fun deleteFavourites(favouritesData: FavouritesData) {
         repository.deleteFavourites(favouritesData)
     }
+
+    fun loadAllFavourites() = repository
+            .getAllFavourites()
+            .map { data ->
+                val cells: ArrayList<ComicsCell> = ArrayList()
+                if (data.isNotEmpty()) {
+                    data.forEach {
+                        val resultResponse = ResultResponse()
+                        resultResponse.id = it.comicsId.toInt()
+                        resultResponse.title = it.title
+                        val uri = Uri.parse(it.imageUrl)
+                        resultResponse.images = arrayListOf(
+                                ImageResponse(uri.toString().replace("/portrait_xlarge.jpg", ""), "jpg")
+                        )
+                        val cell = ComicsCell(resultResponse)
+                        cells.add(cell)
+                    }
+                }
+                return@map cells
+            }
 }
